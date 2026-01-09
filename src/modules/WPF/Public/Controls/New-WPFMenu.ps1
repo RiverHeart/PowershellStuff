e5539fc6e596d128e41bd8be3b47482ca9c8abb8
Riverheart <RiverHeart@users.noreply.github.com>
2026-01-09 00:58:43 -0500

    Add WPF Menu notes

--- src/modules/WPF/Public/Controls/New-WPFMenu.ps1
+++ src/modules/WPF/Public/Controls/New-WPFMenu.ps1
@@ -2,6 +2,29 @@
 .SYNOPSIS
     Creates a WPF Menu object.
 
+.NOTES
+    I would like to find a way to support this simplified syntax
+    which omits the DockPanel and uses path separators to reduce the
+    nesting. This assumes that the scriptblock for a menuitem is always
+    going to be a scriptblock for Execute or an OnClick handler.
+    Unfortunately there doesn't seem to be a great way of supporting
+    Target/CanExecute with this...
+
+    MenuBar 'Menu' {
+        $self.Height = 25
+
+        MenuItem '_File/_Open' {
+            Command
+        }
+        MenuItem '_File/_Exit' {
+            Write-Host "Barfu"
+        }
+
+        MenuItem '_Help/_Abount' {
+            Write-Host "Zanzibar"
+        }
+    }
+
 .LINK
     https://learn.microsoft.com/en-us/dotnet/api/system.windows.controls.menu
 #>
@@ -18,6 +41,7 @@ function New-WPFMenu {
     )
 
     try {
+        # TODO: Implicitly add a DockPanel here
         $WPFObject = [System.Windows.Controls.Menu] @{
             Name = $Name
         }
