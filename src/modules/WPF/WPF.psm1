2f217b66ff667193e584071d1e78000faf2593eb
Riverheart <RiverHeart@users.noreply.github.com>
2026-06-06 09:06:32 -0400

    Fix module export behavior for WPF

--- src/modules/WPF/WPF.psm1
+++ src/modules/WPF/WPF.psm1
@@ -1,5 +1,12 @@
 $ModuleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 
+# NOTE: I should probably separate classes from functions and load them first to avoid
+# issues. I could also require classes to be named like `*.class.ps1` to make it
+# easier to identify and load them first.
+
+# Populate Module Scope
+#------------------------
+
 $script:LastDialogResult = $false
 $script:LastDialogCloseReason = 'Unknown'
 
@@ -46,3 +53,24 @@ foreach ($Path in $Paths) {
             . $_.FullName
         }
 }
+
+# Export Resources
+#-----------------
+
+# `psm1` exports everything by default instead of respecting the `psd1` manifest,
+# as you'd expect, unless `Export-ModuleMember` is used to explicitly specify exports.
+# As a workaround, load the manifest and use that to determine what to export.
+# Fortunately, aliases defined by attribute decorators are still exported by default.
+$ManifestPath = Join-Path -Path $ModuleRoot -ChildPath 'WPF.psd1'
+$Manifest = Import-PowerShellDataFile -Path $ManifestPath
+
+$FunctionsToExport = if ($Manifest.ContainsKey('FunctionsToExport')) { $Manifest.FunctionsToExport } else { @() }
+$CmdletsToExport = if ($Manifest.ContainsKey('CmdletsToExport')) { $Manifest.CmdletsToExport } else { @() }
+$VariablesToExport = if ($Manifest.ContainsKey('VariablesToExport')) { $Manifest.VariablesToExport } else { @() }
+$AliasesToExport = if ($Manifest.ContainsKey('AliasesToExport')) { $Manifest.AliasesToExport } else { @() }
+
+Export-ModuleMember `
+    -Function $FunctionsToExport `
+    -Cmdlet $CmdletsToExport `
+    -Variable $VariablesToExport `
+    -Alias $AliasesToExport
