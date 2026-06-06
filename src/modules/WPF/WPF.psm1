c8749a7d5bdb7eda2f4373ea59b633b8afb2c9d7
Riverheart <RiverHeart@users.noreply.github.com>
2026-06-06 09:48:31 -0400

    Update commentary on script module export behavior

--- src/modules/WPF/WPF.psm1
+++ src/modules/WPF/WPF.psm1
@@ -57,10 +57,25 @@ foreach ($Path in $Paths) {
 # Export Resources
 #-----------------
 
-# `psm1` exports everything by default instead of respecting the `psd1` manifest,
-# as you'd expect, unless `Export-ModuleMember` is used to explicitly specify exports.
-# As a workaround, load the manifest and use that to determine what to export.
-# Fortunately, aliases defined by attribute decorators are still exported by default.
+# The behavior of Powershell script module importing seems to be:
+#
+# 1) The `psm1` exports everything by default unless `Export-ModuleMember` is used to
+# explicitly specify exports. On the surface, it does not appear to respect the `psd1`
+# manifest as verbose output shows clearly that it exports everything.
+#
+# 2) However, it also appears to be true that if `psd1` specifies exports, then everything
+# not explicitly listed is pruned from the `psm1` exports. This is has to be some dumb legacy
+# behavior but what it boils down to is that if you want verbose output to accurately reflect
+# what is being exported from the module, then you have to load the manifest in the script
+# module and use that to drive `Export-ModuleMember`.
+#
+# So is this necessary? No, but it bothers me that Powershell is being misleading about what is
+# being exported. As a seasoned Powershell user this is confusing and I'm sure that AI agents
+# will also struggle with this. So for the sake of clarity and to avoid confusion, I'm going
+# to do it this way until it becomes an issue, even if it is redundant.
+#
+# Fortunately, aliases defined by attribute decorators are still exported by default so those
+# don't need to be explicitly listed in the manifest.
 $ManifestPath = Join-Path -Path $ModuleRoot -ChildPath 'WPF.psd1'
 $Manifest = Import-PowerShellDataFile -Path $ManifestPath
 
