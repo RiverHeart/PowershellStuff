f01be816b0f2d590bc396815e7dbbca7de23dea1
Riverheart <RiverHeart@users.noreply.github.com>
2026-06-13 22:08:42 -0400

    Update README example

--- src/modules/WPF/Examples/ReadmeExample/ReadmeExample.ps1
+++ src/modules/WPF/Examples/ReadmeExample/ReadmeExample.ps1
@@ -18,75 +18,137 @@ Import-Module ../.. -Force
 Import "../ImageViewer/ImageViewer.styles.ps1"
 Import "../ImageViewer/functions"
 
-Window 'Window' {
+App 'Window' {
     $this.Title = 'Image Viewer'
     $this.WindowStartupLocation = [WindowStartupLocation]::CenterScreen
     $this.AllowDrop = $true
     $this.WindowState = [WindowState]::Maximized
-    $this.Tag = New-WPFObservableState @{
-        IsFullScreen  = $false
-        IsFileLoaded  = $false
-        IsFitMode     = $true
-        ZoomLevel     = 1.0
+    State @{
+        IsFullScreen = $false
+        IsFileLoaded = $false
+        IsFitMode = $true
+        ZoomLevel = 1.0
         RotationAngle = 0
-        CurrentTheme  = if (Get-WPFDarkModePreference) { 'Dark' } else { 'Light' }
+
+        CurrentTheme = if (Get-WPFDarkModePreference) { 'Dark' } else { 'Light' }
         FileNavigator = $null
     }
 
     Use-WPFTheme -Name $this.Tag.CurrentTheme -Root $this
 
-    When KeyDown {
+    Key 'Escape' {
+        if (-not $this.Tag.IsFullScreen) {
+            return
+        }
+
+        Set-WPFWindowFullScreen -IsFullScreen $false
+        if ($this.Tag.IsFitMode) {
+            Invoke-ImageViewerFitToWindow
+        }
+        $event.Handled = $true
+    }
+
+    # Use PreviewKeyDown so navigation still works when focused controls handle KeyDown internally.
+    When PreviewKeyDown {
         param($sender, $event)
 
         switch ($event.Key) {
-            'Escape' {
-                if ((Reference 'Window').Tag.IsFullScreen) {
-                    Set-WPFWindowFullScreen -IsFullScreen $false
-                    Invoke-ImageViewerFitToWindow
+            'Left' {
+                if (Test-ImageViewerShouldNavigate) {
+                    Invoke-ImageViewerNavigate -Direction Back
                     $event.Handled = $true
                 }
-            }
-            'Left' {
-                Invoke-ImageViewerNavigate -Direction Back
-                $event.Handled = $true
+                break
             }
             { $_ -in @('Right', 'Space') } {
-                Invoke-ImageViewerNavigate -Direction Forward
-                $event.Handled = $true
+                if ($event.Key -eq [System.Windows.Input.Key]::Space -or (Test-ImageViewerShouldNavigate)) {
+                    Invoke-ImageViewerNavigate -Direction Forward
+                    $event.Handled = $true
+                }
+                break
             }
         }
     }
 
-    Grid 'Body' {
-        Row {
-            Column 'Expand' {
-                MenuBar 'Menu' {
-                    Bind Visibility -To Window.Tag.IsFullScreen -Invert
+    MenuBar 'Menu' {
+        $this.Height = 25
+        Bind Visibility -To Window.Tag.IsFullScreen -Invert
 
-                    MenuItem '(F)ile/(O)pen' {
-                        Command 'Open' {
-                            $fileName = Get-WPFFileSelection -Category Image -Window (Reference 'Window')
-                            if ($fileName) {
-                                Invoke-ImageViewerLoadFile -FileName $fileName
-                            }
-                        }
-                    }
+        MenuItem '(F)ile/(O)pen' {
+            Command 'Open' {
+                $Window = Get-WPFWindow
+                $FileName = Get-WPFFileSelection -Category Image -Window $Window
+
+                if (-not $FileName) {
+                    return
                 }
+
+                Invoke-ImageViewerLoadFile -FileName $FileName
             }
         }
 
-        Row 'Expand' {
-            Column {
-                ScrollViewer 'ScrollViewer' {
-                    $this.VerticalScrollbarVisibility = [ScrollBarVisibility]::Auto
-                    $this.HorizontalScrollbarVisibility = [ScrollBarVisibility]::Auto
-                    $this.Background = 'Transparent'
+        MenuItem '(V)iew/(F)ullScreen' {
+            Command 'FullScreen' 'F11' {
+                $Window = Get-WPFWindow
+                $State = $Window.Tag
+                $IsEnteringFullScreen = -not $State.IsFullScreen
 
-                    Image 'Viewer' {
-                        $this.VerticalAlignment = [VerticalAlignment]::Center
-                        $this.StretchDirection = [StretchDirection]::DownOnly
-                    }
+                Set-WPFWindowFullScreen -IsFullScreen $IsEnteringFullScreen
+
+                if ($State.IsFitMode) {
+                    Invoke-ImageViewerFitToWindow
                 }
+            }
+        }
+    }
+
+    Content {
+        DockPanel 'MainPanel' {
+            $this.LastChildFill = $true
+
+            ScrollViewer 'ScrollViewer' {
+                $this.VerticalScrollbarVisibility = [ScrollBarVisibility]::Auto
+                $this.HorizontalScrollbarVisibility = [ScrollBarVisibility]::Auto
+                $this.Background = 'Transparent'
+
+                Image 'Viewer' {
+                    $this.VerticalAlignment = [VerticalAlignment]::Center
+                    $this.StretchDirection = [StretchDirection]::DownOnly
+                }
+            }
+        }
+    }
+
+    Footer {
+        StackPanel 'ButtonPanel' {
+            $this.Orientation = [Orientation]::Horizontal
+            $this.HorizontalAlignment = [HorizontalAlignment]::Center
+            Bind Visibility -To Window.Tag.IsFullScreen -Invert
+
+            Button 'BackButton' {
+                Bind IsEnabled -To Window.Tag.IsFileLoaded
+                When 'Click' { Invoke-ImageViewerNavigate -Direction Back }
+            }
+
+            Button 'ForwardButton' {
+                Bind IsEnabled -To Window.Tag.IsFileLoaded
+                When 'Click' { Invoke-ImageViewerNavigate -Direction Forward }
+            }
+        }
+    }
+
+    StatusBar {
+        Bind Visibility -To Window.Tag.IsFullScreen -Invert
+
+        StatusBarItem 'StatusFileItem' {
+            Label 'StatusFileLabel' {
+                $this.Content = 'No image loaded'
+            }
+        }
+
+        StatusBarItem 'StatusZoomItem' {
+            Label 'StatusZoomLabel' {
+                $this.Content = '100%'
             }
         }
     }
