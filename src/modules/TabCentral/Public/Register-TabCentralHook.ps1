165032e948c8d5b5bbdd2650408f0535b0e99b2f
Riverheart <RiverHeart@users.noreply.github.com>
2026-07-18 17:19:20 -0400

    Support registration of tab hooks from module metadata

--- src/modules/TabCentral/Public/Register-TabCentralHook.ps1
+++ src/modules/TabCentral/Public/Register-TabCentralHook.ps1
@@ -19,10 +19,10 @@
     Get-WPFTabCentralHook | Register-TabCentralHook -Force
 #>
 function Register-TabCentralHook {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Default')]
     [OutputType([void], [pscustomobject])]
     param (
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory,ParameterSetName='Default')]
         [ValidateScript({
             $_ -is [string] -or
             $_ -is [scriptblock] -or
@@ -31,23 +31,133 @@ function Register-TabCentralHook {
         })]
         [object] $Callable,
 
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory,ParameterSetName='Default')]
         [ValidateSet('Completer', 'Modifier')]
         [string] $Type,
 
-        [Parameter(HelpMessage='The name of the hook. Mandatory only when using a scriptblock')]
+        [Parameter(HelpMessage='The name of the hook. Mandatory only when using a scriptblock',ParameterSetName='Default')]
         [ValidateNotNullOrEmpty()]
         [string] $Name,
 
-        [Parameter(HelpMessage='The source of the hook. Mandatory only when using a scriptblock')]
+        [Parameter(HelpMessage='The source of the hook. Mandatory only when using a scriptblock',ParameterSetName='Default')]
         [ValidateNotNullOrEmpty()]
         [string] $Source,
+
+        [Parameter(Mandatory,ParameterSetName='Module')]
+        [ValidateScript({
+            $_ -is [string] -or
+            $_ -is [System.Management.Automation.PSModuleInfo]
+        })]
+        [object] $Module,
+
+        [Parameter(ParameterSetName='Module')]
+        [version] $Version,
 
         [switch] $Force,
         [switch] $PassThru
     )
 
     process {
+        if ($PSCmdlet.ParameterSetName -eq 'Module') {
+            $ResolvedModule = $null
+
+            if ($Module -is [string]) {
+                $LoadedMatches = @(
+                    Get-Module -Name $Module -All | Sort-Object Version -Descending
+                )
+                if ($Version) {
+                    $LoadedMatches = @($LoadedMatches | Where-Object { $_.Version -eq $Version })
+                }
+
+                if ($LoadedMatches.Count -gt 0) {
+                    $ResolvedModule = $LoadedMatches[0]
+                } else {
+                    $AvailableMatches = @(
+                        Get-Module -ListAvailable -Name $Module | Sort-Object Version -Descending
+                    )
+                    if ($Version) {
+                        $AvailableMatches = @($AvailableMatches | Where-Object { $_.Version -eq $Version })
+                    }
+
+                    if ($AvailableMatches.Count -eq 0) {
+                        if ($Version) {
+                            Write-Error "Module '$Module' with version '$Version' was not found." -Category ObjectNotFound
+                        } else {
+                            Write-Error "Module '$Module' was not found." -Category ObjectNotFound
+                        }
+                        return
+                    }
+
+                    $ResolvedModule = $AvailableMatches[0]
+
+                    if ($AvailableMatches.Count -gt 1) {
+                        Write-Verbose (
+                            "Multiple module versions found for '$Module'. " +
+                            "Using '$($ResolvedModule.Name)' version '$($ResolvedModule.Version)' at '$($ResolvedModule.Path)'."
+                        )
+                    }
+                }
+            } else {
+                $ResolvedModule = $Module
+
+                if ($Version -and $ResolvedModule.Version -ne $Version) {
+                    Write-Error (
+                        "Provided module '$($ResolvedModule.Name)' version '$($ResolvedModule.Version)' " +
+                        "does not match requested version '$Version'."
+                    ) -Category InvalidArgument
+                    return
+                }
+            }
+
+            if (-not (Get-Module -Name $ResolvedModule.Name -All | Where-Object { $_.Path -eq $ResolvedModule.Path })) {
+                if (-not $ResolvedModule.Path) {
+                    Write-Error "Module '$($ResolvedModule.Name)' is not loaded and has no path to import from." -Category InvalidArgument
+                    return
+                }
+
+                $ImportParams = @{
+                    Name = $ResolvedModule.Path
+                    ErrorAction = 'Stop'
+                }
+                $null = Import-Module @ImportParams
+                $ResolvedModule = Get-Module -Name $ResolvedModule.Name | Sort-Object Version -Descending | Select-Object -First 1
+            }
+
+            $TabExpansion = $ResolvedModule.PrivateData.TabExpansion
+
+            $Completers = @($TabExpansion.Completers)
+            $Modifiers = @($TabExpansion.Modifiers)
+
+            if ($Completers.Count -eq 0 -and $Modifiers.Count -eq 0) {
+                Write-Error "No tab completers or modifiers found in module '$($ResolvedModule.Name)'." -Category InvalidData
+                return
+            }
+
+            foreach($Completer in $Completers) {
+                $ChildParams = @{
+                    Name = $Completer
+                    Type = 'Completer'
+                    Callable = $Completer
+                    Force = $Force
+                    PassThru = $PassThru
+                }
+                Register-TabCentralHook @ChildParams
+            }
+
+            foreach($Modifier in $Modifiers) {
+                $ChildParams = @{
+                    Name = $Modifier
+                    Type = 'Modifier'
+                    Callable = $Modifier
+                    Force = $Force
+                    PassThru = $PassThru
+                }
+                Register-TabCentralHook @ChildParams
+            }
+
+            return
+        }
+
         $HookParams = $PSBoundParameters
         $null = $HookParams.Remove('PassThru')
         $null = $HookParams.Remove('Force')
--- src/modules/TabCentral/Tests/Register-TabCentralHook.tests.ps1
+++ src/modules/TabCentral/Tests/Register-TabCentralHook.tests.ps1
@@ -122,4 +122,207 @@ Describe 'Register-TabCentralHook' {
             } -ErrorAction Stop
         } | Should -Throw
     }
+
+    It 'registers hooks from module PrivateData using an in-memory module' {
+        $ModuleName = 'TabCentral.TestProvider'
+        $ProviderModule = New-Module -Name $ModuleName -ScriptBlock {
+            $ExecutionContext.SessionState.Module.PrivateData = @{
+                TabExpansion = @{
+                    Completers = @('Complete-TabCentralTestProvider')
+                    Modifiers = @('Modify-TabCentralTestProvider')
+                }
+            }
+
+            function Complete-TabCentralTestProvider {
+                param (
+                    [string] $inputScript,
+                    [int] $cursorColumn,
+                    [System.Management.Automation.Language.Ast] $ast,
+                    [System.Management.Automation.Language.Token[]] $tokens,
+                    [System.Management.Automation.Language.IScriptPosition] $positionOfCursor,
+                    [hashtable] $options
+                )
+            }
+
+            function Modify-TabCentralTestProvider {
+                param (
+                    [System.Management.Automation.CommandCompletion] $CommandCompletion
+                )
+
+                return $CommandCompletion
+            }
+
+            Export-ModuleMember -Function Complete-TabCentralTestProvider, Modify-TabCentralTestProvider
+        }
+
+        Import-Module -ModuleInfo $ProviderModule -Force
+        try {
+            Register-TabCentralHook -Module $ProviderModule
+
+            InModuleScope TabCentral {
+                $Registry = Get-TabCentralRegistry
+                $Registry.TabCompleters.ContainsKey('Complete-TabCentralTestProvider') | Should -BeTrue
+                $Registry.ResultModifiers.ContainsKey('Modify-TabCentralTestProvider') | Should -BeTrue
+            }
+        } finally {
+            Remove-Module -Name $ModuleName -ErrorAction SilentlyContinue
+        }
+    }
+
+    It 'returns registered hooks from module mode when PassThru is specified' {
+        $ModuleName = 'TabCentral.TestProvider.PassThru'
+        $ProviderModule = New-Module -Name $ModuleName -ScriptBlock {
+            $ExecutionContext.SessionState.Module.PrivateData = @{
+                TabExpansion = @{
+                    Completers = @('Complete-TabCentralTestProviderPassThru')
+                    Modifiers = @('Modify-TabCentralTestProviderPassThru')
+                }
+            }
+
+            function Complete-TabCentralTestProviderPassThru {
+                param (
+                    [string] $inputScript,
+                    [int] $cursorColumn,
+                    [System.Management.Automation.Language.Ast] $ast,
+                    [System.Management.Automation.Language.Token[]] $tokens,
+                    [System.Management.Automation.Language.IScriptPosition] $positionOfCursor,
+                    [hashtable] $options
+                )
+            }
+
+            function Modify-TabCentralTestProviderPassThru {
+                param (
+                    [System.Management.Automation.CommandCompletion] $CommandCompletion
+                )
+
+                return $CommandCompletion
+            }
+
+            Export-ModuleMember -Function Complete-TabCentralTestProviderPassThru, Modify-TabCentralTestProviderPassThru
+        }
+
+        Import-Module -ModuleInfo $ProviderModule -Force
+        try {
+            $Hooks = @(Register-TabCentralHook -Module $ProviderModule -PassThru)
+
+            $Hooks.Count | Should -Be 2
+            @($Hooks.Name) | Should -Contain 'Complete-TabCentralTestProviderPassThru'
+            @($Hooks.Name) | Should -Contain 'Modify-TabCentralTestProviderPassThru'
+        } finally {
+            Remove-Module -Name $ModuleName -ErrorAction SilentlyContinue
+        }
+    }
+
+    It 'throws on duplicate module registration unless Force is specified' {
+        $ModuleName = 'TabCentral.TestProvider.Force'
+        $ProviderModule = New-Module -Name $ModuleName -ScriptBlock {
+            $ExecutionContext.SessionState.Module.PrivateData = @{
+                TabExpansion = @{
+                    Completers = @('Complete-TabCentralTestProviderForce')
+                    Modifiers = @()
+                }
+            }
+
+            function Complete-TabCentralTestProviderForce {
+                param (
+                    [string] $inputScript,
+                    [int] $cursorColumn,
+                    [System.Management.Automation.Language.Ast] $ast,
+                    [System.Management.Automation.Language.Token[]] $tokens,
+                    [System.Management.Automation.Language.IScriptPosition] $positionOfCursor,
+                    [hashtable] $options
+                )
+            }
+
+            Export-ModuleMember -Function Complete-TabCentralTestProviderForce
+        }
+
+        Import-Module -ModuleInfo $ProviderModule -Force
+        try {
+            Register-TabCentralHook -Module $ProviderModule
+
+            {
+                Register-TabCentralHook -Module $ProviderModule -ErrorAction Stop
+            } | Should -Throw
+
+            {
+                Register-TabCentralHook -Module $ProviderModule -Force -ErrorAction Stop
+            } | Should -Not -Throw
+        } finally {
+            Remove-Module -Name $ModuleName -ErrorAction SilentlyContinue
+        }
+    }
+
+    It 'supports version filtering when module info version matches' {
+        $ModuleName = 'TabCentral.TestProvider.Version.Match'
+        $ProviderModule = New-Module -Name $ModuleName -ScriptBlock {
+            $ExecutionContext.SessionState.Module.PrivateData = @{
+                TabExpansion = @{
+                    Completers = @('Complete-TabCentralTestProviderVersionMatch')
+                    Modifiers = @()
+                }
+            }
+
+            function Complete-TabCentralTestProviderVersionMatch {
+                param (
+                    [string] $inputScript,
+                    [int] $cursorColumn,
+                    [System.Management.Automation.Language.Ast] $ast,
+                    [System.Management.Automation.Language.Token[]] $tokens,
+                    [System.Management.Automation.Language.IScriptPosition] $positionOfCursor,
+                    [hashtable] $options
+                )
+            }
+
+            Export-ModuleMember -Function Complete-TabCentralTestProviderVersionMatch
+        }
+
+        Import-Module -ModuleInfo $ProviderModule -Force
+        try {
+            {
+                Register-TabCentralHook -Module $ProviderModule -Version $ProviderModule.Version -ErrorAction Stop
+            } | Should -Not -Throw
+
+            InModuleScope TabCentral {
+                $Registry = Get-TabCentralRegistry
+                $Registry.TabCompleters.ContainsKey('Complete-TabCentralTestProviderVersionMatch') | Should -BeTrue
+            }
+        } finally {
+            Remove-Module -Name $ModuleName -ErrorAction SilentlyContinue
+        }
+    }
+
+    It 'throws when module info version does not match requested version' {
+        $ModuleName = 'TabCentral.TestProvider.Version.Mismatch'
+        $ProviderModule = New-Module -Name $ModuleName -ScriptBlock {
+            $ExecutionContext.SessionState.Module.PrivateData = @{
+                TabExpansion = @{
+                    Completers = @('Complete-TabCentralTestProviderVersionMismatch')
+                    Modifiers = @()
+                }
+            }
+
+            function Complete-TabCentralTestProviderVersionMismatch {
+                param (
+                    [string] $inputScript,
+                    [int] $cursorColumn,
+                    [System.Management.Automation.Language.Ast] $ast,
+                    [System.Management.Automation.Language.Token[]] $tokens,
+                    [System.Management.Automation.Language.IScriptPosition] $positionOfCursor,
+                    [hashtable] $options
+                )
+            }
+
+            Export-ModuleMember -Function Complete-TabCentralTestProviderVersionMismatch
+        }
+
+        Import-Module -ModuleInfo $ProviderModule -Force
+        try {
+            {
+                Register-TabCentralHook -Module $ProviderModule -Version ([version]'999.0') -ErrorAction Stop
+            } | Should -Throw
+        } finally {
+            Remove-Module -Name $ModuleName -ErrorAction SilentlyContinue
+        }
+    }
 }
