265cd7a11a7e74a4c67a19a29c48967bf0d0e5de
Riverheart <RiverHeart@users.noreply.github.com>
2026-08-08 15:33:58 -0400

    Add install task

--- src/modules/PleaseWork/taskfile.ps1
+++ src/modules/PleaseWork/taskfile.ps1
@@ -22,3 +22,24 @@ lint: changed('./**/*.ps1') {
 test: changed('./src', './Tests') {
     & "$GitRoot/tools/Invoke-Test.ps1" -TestSuite PleaseWork
 }
+
+install: {
+    if ($null -ne $env:VSCODE_INJECTION) {
+        Write-Warning "Skipping install in VSCode injection context."
+        return
+    }
+    $Project = Import-PowerShellDataFile -LiteralPath "$TaskFileRoot/project.psd1"
+    $Project.DevDependencies.GetEnumerator() | ForEach-Object {
+        #Write-Host "Install-Module -Name $($_.Key) -RequiredVersion $($_.Value) -Scope CurrentUser"
+        $InstallParams = @{
+            Name = $_.Key
+            Scope = 'CurrentUser'
+        }
+        if ($_.Value.StartsWith('^')) {
+            $InstallParams.MinimumVersion = $_.Value.TrimStart('^')
+        } else {
+            $InstallParams.RequiredVersion = $_.Value
+        }
+        Install-Module @InstallParams
+    }
+}
